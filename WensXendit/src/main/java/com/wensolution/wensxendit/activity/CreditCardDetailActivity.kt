cc618fec9e05a55c93d3e7487280c6c895d2a0fa
wensolution <antonizheng99@gmail.com>
2023-04-18 09:50:07 +0700

    v 0.0.3

--- WensXendit/src/main/java/com/wensolution/wensxendit/activity/CreditCardDetailActivity.kt
+++ WensXendit/src/main/java/com/wensolution/wensxendit/activity/CreditCardDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import com.wensolution.wensxendit.IntentKey
 import com.wensolution.wensxendit.activity.viewmodel.CreditCardViewModel
 import com.wensolution.wensxendit.ProgressDialog
 import com.wensolution.wensxendit.activity.viewmodel.ViewModelFactory
@@ -23,6 +24,7 @@ class CreditCardDetailActivity : AppCompatActivity() {
 
         val total = intent.getLongExtra("TOTAL_PAY", 0)
         val username = intent.getStringExtra("USERNAME") ?: ""
+        val referenceId = intent.getStringExtra(IntentKey.REFERENCE_ID) ?: ""
 
         val progressDialog = ProgressDialog(this)
 
@@ -69,7 +71,7 @@ class CreditCardDetailActivity : AppCompatActivity() {
                 cardholderName = holderName
             )
             val card = CardRequestBody.Card(cardInformation = information)
-            val requestBody = CardRequestBody(card = card)
+            val requestBody = CardRequestBody(card = card, referenceId = referenceId)
 
             if (cardNumber.isNotBlank() && month.isNotBlank() && year.isNotBlank() && cvv.isNotBlank() && holderName.isNotBlank()) {
                 viewModel.createCardMethods(requestBody)
